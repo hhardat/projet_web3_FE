38c7e1c998f383bb63607a5e321abfca4b1efcbf
Assaidi Anis <anis.assaidi@student.vinci.be>
2022-11-19 18:55:14 +0100

    try product

--- standardreactapp/src/components/App/App.js
+++ standardreactapp/src/components/App/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import NavBar from '../NavBar/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from '../Home/Home';
+import Css from './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <NavBar />
-    </div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <Home> </Home>
+      </div>
   );
 }
 
