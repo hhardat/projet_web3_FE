367cff13a0dd05bbd7d56484b77d63508ec053aa
CedricNiyiVinci <cedric.niyikiza@student.vinci.be>
2022-11-19 15:38:55 +0100

    refactor - default first page application

---
+++ nextjsreactapp/src/pages/index.js
@@ -0,0 +1,22 @@
+const HomePage =() => {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Hello World
+          </p>
+          <a
+            className="App-link"
+            href="https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/HelloWorld.svg/1200px-HelloWorld.svg.png"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            📷
+          </a>
+        </header>
+      </div>
+    );
+  }
+  
+  export default HomePage;
+  
