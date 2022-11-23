8fe9e64842f3386aa762d3eddf184a1133545267
CedricNiyiVinci <cedric.niyikiza@student.vinci.be>
2022-11-24 00:42:17 +0100

    refactor - use of SSR

--- nextjsreactapp/src/pages/products.js
+++ nextjsreactapp/src/pages/products.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import axios from 'axios'
 import ProductList from "components/Product/ProductList";
 
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
   const result = await axios.get("http://localhost:3001/products")
   return {
     props: {
