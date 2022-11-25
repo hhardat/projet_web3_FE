3cfb721b6f6209212cc64b0652e9f9cd88f13e2d
anis assaidi <anis.assaidi@student.vinci.be>
2022-11-25 16:55:54 +0100

    delet comment in home

--- standardreactapp/src/components/Home/Home.jsx
+++ standardreactapp/src/components/Home/Home.jsx
@@ -9,34 +9,4 @@ const Home = ({ products }) => {
   )
 }
   export default Home;
-  /*
-   items.map((item)=> (
-    
-   <Container>
-   <Row className="justify-content-md-center">
-    <Col xs={6}>
-    
-    <Card ClassName="h-100">
-    <Card.Img variant="top" src={item.imgUrl}  height="400" style={{objectFit: "cover" }} />
-    
-    <Card.Body  className="d-flex flex-column">
-    <Card.Title className="d-flex justify-content-between align-items-basline mb-4">
-
-   <span className="fs-2"> <h1>{item.name}</h1> </span> 
-   <span className="ms-2 text-muted"><h2>{item.price} $</h2></span>
-    </Card.Title>
-   
-    </Card.Body>
-    </Card>
-   
-
-    <Button variant="secondary" size="lg">
-        BUY
-      </Button>
-      </Col>
-      </Row>
-   
-    
-      </Container>
-    
-  ));*/
+  
