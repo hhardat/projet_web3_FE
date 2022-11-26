aa4fa773937076567973b49f4cdacc458825718f
CedricNiyiVinci <cedric.niyikiza@student.vinci.be>
2022-11-26 20:22:11 +0100

    refactor - add Navbar to app

---
+++ nextjsreactapp/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const NavBar = () => {
+
+  const [categories, setCategories] = useState([]);
+  // recupération de la dat GET 
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/categories')
+      .then(response => {
+        console.log('promise fulfilled')
+        //setTimeout(() => setNotes(response.data), 5000)
+        setCategories(response.data)
+        console.log(categories)
+      })
+  }
+  
+  useEffect(hook, [])
+  
+  return (
+      <Navbar bg="black" variant="dark">
+        <Container fluid>
+                <Navbar.Brand href="/">Shop</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">All</Nav.Link>
+                        <NavDropdown title="Link" id="navbarScrollingDropdown" >
+                            {categories.map((category)=>(
+                                <NavDropdown.Item key={category.id} href={`/categories/${category.slug}`}>{category.name}</NavDropdown.Item>))}
+                        </NavDropdown>
+                    </Nav>
+                   
+        </Container>
+      </Navbar>
+  );
+}
+
+export default NavBar;
--- nextjsreactapp/src/pages/_app.js
+++ nextjsreactapp/src/pages/_app.js
@@ -1,9 +1,17 @@
 import '../style/global.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
+import Layout from "components/Layout/Layout";
+import SSRProvider from 'react-bootstrap/SSRProvider';
+
 const App = ({Component, pageProps}) => {
-    return (
-      <Component {...pageProps}/>);
+    return ( 
+      <SSRProvider>
+        <Layout>
+          <Component {...pageProps}/>
+        </Layout>
+      </SSRProvider>
+    );
   }
   
   export default App;
